48287a1043a577048b352abed92c2571419dbced
singhvishesh9484 <visheshsingh084@gmail.com>
2022-08-25 22:44:20 +0530

    homePage day-3

--- monsterindia/src/App.js
+++ monsterindia/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from "react"
 import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar/>
+      <HomePage/>
     </div>
   );
 }
--- monsterindia/src/index.js
+++ monsterindia/src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 import {store} from "./Redux/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <Provider store={store}>
-        <App />
-    </Provider>
+        <Provider store={store}>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </Provider>
       
     </BrowserRouter>
   
