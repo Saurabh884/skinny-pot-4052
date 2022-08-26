56ac517343594722846584e9911461df6e062d48
singhvishesh9484 <visheshsingh084@gmail.com>
2022-08-26 15:17:12 +0530

    day3 change

---
+++ monsterindia/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from "react"
+import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
