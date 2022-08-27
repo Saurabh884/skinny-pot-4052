453adca09c111c9e1b82c83b753301ce66f60565
Pushpendra Kumar <kumarpushp1992@gmail.com>
2022-08-27 14:45:06 +0530

    day5,signup

--- monsterindia/src/Pages/MainRoutes.jsx
+++ monsterindia/src/Pages/MainRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import {Login} from "./Login";
 import Premium_Services from './Premium_Services';
+import Signup from "./Signup";
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/premium" element = {<Premium_Services/>} />
+      <Route path="/signup" element = {<Signup/>} />
       {/* <Route path="/signup" element={<Signup />} /> */}
     </Routes>
   );
